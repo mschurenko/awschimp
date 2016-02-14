fb19a967b36a120a56a53750f36e22d091f3cc60
Matt Schurenko <mschurenko@demonware.net>
2016-02-14 11:24:59 -0800

    adding awschimp

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+import awschimp
+
+setup(
+    name="awschimp",
+    version=aws_simp.__version__,
+    description="aws utils fit for a chimp",
+    author="Matt Schurenko",
+    author_email="matt.schurenko@gmail.com",
+    url="https://github.com/mschurenko/awschimp",
+    license="MIT",
+    packages=["awschimp"],
+    keywords=["aws", "sts", "boto", "profile", "awschimp", ],
+    install_requires=[
+        "boto3",
+        "botocore",
+    ],
+    zip_safe=False
+)

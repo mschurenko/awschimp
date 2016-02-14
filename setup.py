893ec8b646b450ba40716886ae053ff985afcb32
Matt Schurenko <mschurenko@demonware.net>
2016-02-14 12:03:31 -0800

    using safer method of getting __version__ in setup

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
-import awschimp
+import os
+
+for f in open(os.path.join(os.path.dirname(__file__),
+              "awschimp", "__init__.py")).readlines():
+    if f.startswith("__version__"):
+        ver = f.split("=")[-1]
 
 setup(
     name="awschimp",
-    version=awschimp.__version__,
+    version=ver,
     description="aws utils fit for a chimp",
     author="Matt Schurenko",
     author_email="matt.schurenko@gmail.com",
